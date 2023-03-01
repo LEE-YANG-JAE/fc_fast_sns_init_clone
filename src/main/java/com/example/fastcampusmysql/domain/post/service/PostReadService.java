d6d091605189126acd1841a59b546960095360fb
LEE-YANG-JAE <33215984+LEE-YANG-JAE@users.noreply.github.com>
2023-03-01 12:41:41 +0900

    2023-03-01 내가 쓴 글 캘린더 구현

---
+++ src/main/java/com/example/fastcampusmysql/domain/post/service/PostReadService.java
@@ -0,0 +1,29 @@
+package com.example.fastcampusmysql.domain.post.service;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class PostReadService {
+
+    final private PostRepository postRepository;
+
+    public List<DailyPostCount> getDailyPostCounts(DailyPostCountRequest request) {
+        /*
+            반환 값 -> 리스트 [작성일자, 작성회원, 작성 게시물 갯수]
+            select createdDate, memberId, count(id)
+            from Post
+            where 1=1
+            and memberId = :memberId
+            group by cratedDate, memberId
+         */
+        return postRepository.groupByCreatedDate(request);
+    }
+}
